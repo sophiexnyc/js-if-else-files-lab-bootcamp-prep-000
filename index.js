fe4efe4250a465d6dcb1c0896500a3473c973cde
Sophie Lee <sblee092@gmail.com>
2019-07-22 21:46:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var name = "Susan"
-var height = "70"
+var height = 70
 
 
 // Don't worry about this
 module.exports = { name, height
-}
-if (height === "70"){
-  console.log("The height is correct")
 }
